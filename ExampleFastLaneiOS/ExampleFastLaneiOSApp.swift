4ca61a8c883a2ebb7612bf76c3d2c1946584bf10
Dilshan Fernando <4301311+dilshan5@users.noreply.github.com>
2022-06-27 14:14:10 +0200

    Add files via upload
    initial commit

---
+++ ExampleFastLaneiOS/ExampleFastLaneiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleFastLaneiOSApp.swift
+//  ExampleFastLaneiOS
+//
+//  Created by Dilshan Fernando on 27/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct ExampleFastLaneiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
