39a9b08a4654e031d3cd107ad37e08e455f6c686
Dilshan Fernando (BB) <dilshan@backbase.com>
2022-08-15 21:13:16 +0200

    initial commit

--- ExampleFastLaneiOSTests/ExampleFastLaneiOSTests.swift
+++ ExampleFastLaneiOSTests/ExampleFastLaneiOSTests.swift
@@ -28,9 +28,6 @@ class ExampleFastLaneiOSTests: XCTestCase {
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
     }
 
 }
